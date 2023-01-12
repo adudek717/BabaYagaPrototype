93e7c97cee560c9d0ff24328539e417a72fa51c2
adudek717 <a.dudek717@gmail.com>
2023-01-12 19:30:51 +0100

    Remove debug output displaying movement sensitivity

--- babayaga.py
+++ babayaga.py
@@ -15,54 +15,54 @@ Autorzy:
     Aleksander Dudek s20155
     Jakub Słomiński  s18552
 """
+
 import cv2
 
-# Load the video file
+# Load the video
 video_file = "video.mp4"
 capture = cv2.VideoCapture(video_file)
 
-# Initialize the face detection classifier
+# Initialize face detection classifier
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Set up the previous frame for the face movement check
+# Set up previous frame for face movement check
 _, prev_frame = capture.read()
 prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
 
 while True:
-    # Read the current frame
+    # Read current frame
     success, frame = capture.read()
 
-    # Check if the video has reached the end
+    # Check if video has reached the end
     if not success:
         break
 
-    # Convert the current frame to grayscale
+    # Convert current frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces in the current frame
+    # Detect faces in current frame
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faces:
-        # Check if the face has moved by comparing the previous frame
+        # Check if face has moved by comparing the previous frame
         face_movement = cv2.absdiff(
             prev_gray[y:y+h, x:x+w], gray[y:y+h, x:x+w])
         if cv2.countNonZero(face_movement) > 17000:
-            # Draw a crosshair on the face
-            print(cv2.countNonZero(face_movement))
+            # Draw crosshair on face
             cv2.line(frame, (x + w//2, y), (x + w//2, y + h), (0, 0, 255), 2)
             cv2.line(frame, (x, y + h//2), (x + w, y + h//2), (0, 0, 255), 2)
 
-    # Show the frame
+    # Show frame
     cv2.imshow("Frame", frame)
 
-    # Update the previous frame
+    # Update previous frame
     prev_gray = gray
 
-    # Wait for the user to press a key
+    # Wait for user to press a key
     key = cv2.waitKey(1)
     if key == 27:  # Esc key
         break
 
-# Release the capture and close the window
+# Release capture and close the window
 capture.release()
 cv2.destroyAllWindows()
